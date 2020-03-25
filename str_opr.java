6d5ef53587118d1cef29838e3c8fa1db1d2c724d
mounica <bobbamounika2000@gmail.com>
2020-03-25 20:43:46 +0530

    commiting a java file

---
+++ str_opr.java
@@ -0,0 +1,22 @@
+package TddJunit;
+import java.util.*;
+public class str_opr
+{
+    public static void main(String args[])
+	{
+          Scanner s=new Scanner(System.in);
+	  String given_str=s.nextLine();
+	  int length=given_str.length();
+	  if(given_str.charAt(0)=='A')
+		{
+		  given_str=given_str.substring(1,length);
+		  length=length-1;
+		}
+	  if(given_str.charAt(0)=='A')
+		{
+		  given_str=given_str.substring(1,length);
+		  length=length-1
+		}
+	  System.out.println(given_str);
+	}
+}
