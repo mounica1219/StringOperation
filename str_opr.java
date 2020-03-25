f01e772f7419bdae91debcf08dfaec28d3053b64
mounica1219 <60290800+mounica1219@users.noreply.github.com>
2020-03-25 21:11:04 +0530

    Update str_opr.java

--- str_opr.java
+++ str_opr.java
@@ -1,22 +1,24 @@
 package TddJunit;
 import java.util.*;
-public class str_opr
+public class Main
 {
     public static void main(String args[])
 	{
           Scanner s=new Scanner(System.in);
 	  String given_str=s.nextLine();
 	  int length=given_str.length();
-	  if(given_str.charAt(0)=='A')
+	   if(given_str.charAt(1)=='A')
+		{
+		  given_str=given_str.charAt(0)+given_str.substring(2,length);
+		  length=length-1;
+		}
+	  if(given_str.charAt(0)=='A' )
 		{
 		  given_str=given_str.substring(1,length);
 		  length=length-1;
 		}
-	  if(given_str.charAt(0)=='A')
-		{
-		  given_str=given_str.substring(1,length);
-		  length=length-1
-		}
-	  System.out.println(given_str);
+		
+	 
+		System.out.println(given_str);
 	}
 }
